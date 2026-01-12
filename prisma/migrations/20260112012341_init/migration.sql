fbbe3efea6e001920deeb0e2968b333af73e4ac0
Rui <s1F102501866@iniad.org>
2026-01-12 10:25:08 +0900

    chore: Prisma(ORM)の設定

---
+++ prisma/migrations/20260112012341_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
